8cdcca5bbbce065d4309b907e0ef38002461afb2
storezhang <storezhang@gmail.com>
2025-04-27 12:48:00 +0800

    refactor(项目): 使用依赖注入重构

--- internal/config/http.go
+++ internal/config/http.go
@@ -8,7 +8,7 @@ type Http struct {
 	Client *Client `default:"{}" json:"client,omitempty" validate:"required"`
 }
 
-func newHttp(config *config.Getter) (http *Http, err error) {
+func newHttp(config config.Getter) (http *Http, err error) {
 	http = new(Http)
 	err = config.Get(&struct {
 		Http *Http `json:"http,omitempty" validate:"required"`
